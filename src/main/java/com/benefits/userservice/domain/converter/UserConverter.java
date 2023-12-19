4501350395228d7e98f19d006e35e6f3ef040e4c
Develing1991 <completed0728@gmail.com>
2023-12-20 01:34:39 +0900

    feat: add toResponse(userAddressList)

--- src/main/java/com/benefits/userservice/domain/converter/UserConverter.java
+++ src/main/java/com/benefits/userservice/domain/converter/UserConverter.java
@@ -1,10 +1,14 @@
 package com.benefits.userservice.domain.converter;
 
 import com.benefits.userservice.common.annotation.Converter;
+import com.benefits.userservice.domain.controller.model.response.UserAddressResponse;
 import com.benefits.userservice.domain.controller.model.response.UserProfileResponse;
 import com.benefits.userservice.domain.controller.model.request.UserRequest;
 import com.benefits.userservice.domain.controller.model.response.UserResponse;
 import com.benefits.userservice.db.entity.users.UserEntity;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Converter
 public class UserConverter {
@@ -30,6 +34,23 @@ public class UserConverter {
                 .userProfileResponse(userProfileResponse)
                 .build();
     }
+    public UserResponse toResponse(UserEntity userEntity,
+                                   UserProfileResponse userProfileResponse,
+                                   List<UserAddressResponse> userAddressResponseList){
+        return UserResponse.builder()
+                .id(userEntity.getId())
+                .name(userEntity.getName())
+                .email(userEntity.getEmail())
+                .phone(userEntity.getPhone())
+                .status(userEntity.getStatus())
+                .registeredAt(userEntity.getRegisteredAt())
+                .unregisteredAt(userEntity.getUnregisteredAt())
+                .lastLoginAt(userEntity.getLastLoginAt())
+                .userProfileResponse(userProfileResponse)
+                .userAddressResponseList(userAddressResponseList)
+                .build();
+    }
+
     public UserResponse toResponse(UserEntity userEntity){
         return UserResponse.builder()
                 .id(userEntity.getId())
