0fb533a56084a06fd06667754cd0428e4e25f3bf
Develing1991 <completed0728@gmail.com>
2023-12-20 18:06:43 +0900

    feat: add swagger config - 보류

---
+++ src/main/java/com/benefits/userservice/config/swagger/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.benefits.userservice.config.swagger;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.core.jackson.ModelResolver;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+/*
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public ModelResolver modelResolver(ObjectMapper objectMapper){
+        return new ModelResolver(objectMapper);
+    }
+
+    @Bean
+    public OpenAPI openAPI(){
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(securityRequirement))
+                .info(new Info()
+                .title("커머스 프로젝트 API")
+                .description("상품을 등록하고, 상품을 장바구니에 담는 기능을 제공합니다.")
+                .version("1.0.0"));
+    }
+}
+*/
