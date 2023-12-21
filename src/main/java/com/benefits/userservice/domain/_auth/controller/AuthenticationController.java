c0eabef45dd8cfc981a935c3bce0516b49ec2d93
Develing1991 <completed0728@gmail.com>
2023-12-21 16:27:22 +0900

    feat: add restore token controller

--- src/main/java/com/benefits/userservice/domain/_auth/controller/AuthenticationController.java
+++ src/main/java/com/benefits/userservice/domain/_auth/controller/AuthenticationController.java
@@ -7,33 +7,19 @@ import com.benefits.userservice.domain._auth.business.AuthenticationBusiness;
 import com.benefits.userservice.domain._auth.model.LoginRequest;
 import com.benefits.userservice.domain._auth.model.TokenResponse;
 import com.benefits.userservice.domain._auth.service.AuthenticationService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
 @RequiredArgsConstructor
 public class AuthenticationController {
 
-   /* private final AuthenticationService authenticationService;
-
-    @PostMapping("/login")
-    public ResponseEntity<TokenResponse> login(@AuthenticationPrincipal UserSession userSession){
-
-        var responseToken = authenticationService.login(userSession);
-
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .header("access_token", responseToken.getAccessToken()) //accessToken은 헤더로 내려줄지 바디로 내려줄지 선택
-                .header(HttpHeaders.SET_COOKIE, responseToken.getResponseCookie().toString())
-                .body(responseToken);
-    }*/
    private final AuthenticationBusiness authenticationBusiness;
    @PostMapping("/login")
    public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest request){
@@ -43,4 +29,12 @@ public class AuthenticationController {
                .header(HttpHeaders.SET_COOKIE, responseToken.getResponseCookie().toString())
                .body(responseToken);
    }
+
+    @GetMapping("/restore")
+    public ResponseEntity<TokenResponse> restore(HttpServletRequest request){
+        var responseToken = authenticationBusiness.restore(request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .header("access_token", responseToken.getAccessToken()) //accessToken은 헤더로 내려줄지 바디로 내려줄지 선택
+                .body(responseToken);
+    }
 }
