070cab2271342aabd01b62ad9e321bc726a38aca
Develing1991 <completed0728@gmail.com>
2023-12-21 16:26:56 +0900

    feat: add restore, validation token, UserSession to UserEntity

--- src/main/java/com/benefits/userservice/domain/_auth/business/AuthenticationBusiness.java
+++ src/main/java/com/benefits/userservice/domain/_auth/business/AuthenticationBusiness.java
@@ -2,18 +2,23 @@ package com.benefits.userservice.domain._auth.business;
 
 import com.benefits.userservice.common.annotation.Business;
 import com.benefits.userservice.common.exception.ApiException;
+import com.benefits.userservice.common.resultcode.TokenResultCode;
 import com.benefits.userservice.common.resultcode.UserResultCode;
 import com.benefits.userservice.config.security.auth.model.UserSession;
 import com.benefits.userservice.domain._auth.model.LoginRequest;
 import com.benefits.userservice.domain._auth.model.TokenResponse;
 import com.benefits.userservice.domain._auth.service.AuthenticationService;
 import com.benefits.userservice.domain.users.service.UserService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Arrays;
 
 @Business
 @RequiredArgsConstructor
@@ -39,7 +44,20 @@ public class AuthenticationBusiness {
 
         // 한번 더 security 에게 인증 확인 및 UserSession(UserDetails) 생성
         Authentication authenticationResponse = authenticationManager.authenticate(authenticationRequest);
-        UserSession userSession = (UserSession) authenticationResponse.getPrincipal();
-        return authenticationService.login(userSession);
+        // UserSession userSession = (UserSession) authenticationResponse.getPrincipal();
+        return authenticationService.login(userEntity);
+    }
+
+    public TokenResponse restore(HttpServletRequest request){
+
+        var token = Arrays.stream(request.getCookies())
+                    .filter(it ->{
+                        return it.getName().equals("refreshToken");
+                    }).findAny().orElseThrow(() -> new ApiException(TokenResultCode.AUTHORIZATION_TOKEN_NOT_FOUND));
+
+        var email = authenticationService.validationToken(token.getValue());
+        var userEntity = userService.getUserByEmailWithThrow(email);
+
+        return authenticationService.restore(userEntity);
     }
 }
