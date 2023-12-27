3af4058b1dbe1fb277edba4c59f8f8cb9bd87ad2
Develing1991 <completed0728@gmail.com>
2023-12-28 05:43:04 +0900

    feat: add api

---
+++ src/main/java/com/benefits/userservice/domain/address/controller/AddressController.java
@@ -0,0 +1,44 @@
+package com.benefits.userservice.domain.address.controller;
+
+import com.benefits.userservice.common.spec.Api;
+import com.benefits.userservice.domain.address.business.UserAddressBusiness;
+import com.benefits.userservice.domain.address.model.UserAddressRequest;
+import com.benefits.userservice.domain.address.model.UserAddressResponse;
+import com.benefits.userservice.domain.users.model.UserResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class AddressController {
+
+    private final UserAddressBusiness userAddressBusiness;
+    @PostMapping("/address")
+    public Api<UserAddressResponse> registerProfile(@RequestBody @Valid UserAddressRequest request) {
+        var response = userAddressBusiness.register(request);
+        return Api.CREATE(response.getData());
+    }
+
+    @GetMapping(value = "/address", params = "userId")
+    public Api<List<UserAddressResponse>> getAllUserAddressByUserId(@RequestParam(value = "userId") Long userId){
+        var response = userAddressBusiness.getAllUserAddressByUserId(userId);
+        return Api.OK(response.getData());
+    }
+
+    @PutMapping(value = "/address/{id}")
+    public Api<UserAddressResponse> updateUser(@PathVariable(value = "id") Long id,
+                                        @RequestBody @Valid UserAddressRequest request){
+        var response = userAddressBusiness.updateAddress(id, request);
+        return Api.OK(response.getData());
+    }
+
+    @DeleteMapping("/address/{id}")
+    public Api<UserResponse> deleteAddress(@PathVariable(name = "id") Long id){
+        userAddressBusiness.deleteAddress(id);
+        return Api.OK(null);
+    }
+}
---
+++ src/main/java/com/benefits/userservice/domain/profiles/controller/ProfileController.java
@@ -0,0 +1,34 @@
+package com.benefits.userservice.domain.profiles.controller;
+
+import com.benefits.userservice.common.spec.Api;
+import com.benefits.userservice.domain.profiles.business.UserProfileBusiness;
+import com.benefits.userservice.domain.profiles.model.UserProfileRequest;
+import com.benefits.userservice.domain.profiles.model.UserProfileResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+
+/*
+GET /profiles?userId={id}
+PUT /profiles/:id*/
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class ProfileController {
+
+    private final UserProfileBusiness userProfileBusiness;
+
+    @GetMapping(value = "/profiles", params = "userId")
+    public Api<UserProfileResponse> getProfileByUserId(@RequestParam(value = "userId") Long userId){
+        var response = userProfileBusiness.getProfileByUserId(userId);
+        return Api.OK(response.getData());
+    }
+
+    @PutMapping("/profiles/{id}")
+    public Api<UserProfileResponse> updateProfile(@PathVariable(name = "id") Long id,
+                                        @RequestBody @Valid UserProfileRequest request){
+        var response = userProfileBusiness.updateProfile(id, request);
+        return Api.OK(response.getData());
+    }
+}
