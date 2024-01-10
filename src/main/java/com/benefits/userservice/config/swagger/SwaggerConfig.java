c78210ef9d268a24879ca9447d6b63d3f10b2fcb
Develing1991 <completed0728@gmail.com>
2024-01-11 05:24:40 +0900

    feat: edit swagger remote server url

--- src/main/java/com/benefits/userservice/config/swagger/SwaggerConfig.java
+++ src/main/java/com/benefits/userservice/config/swagger/SwaggerConfig.java
@@ -1,5 +1,5 @@
 package com.benefits.userservice.config.swagger;
-/*
+
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.core.jackson.ModelResolver;
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -27,13 +28,17 @@ public class SwaggerConfig {
                 .in(SecurityScheme.In.HEADER).name("Authorization");
         SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
 
-        return new OpenAPI()
+        var server = new Server();
+        server.setUrl("http://localhost:8000/user-service");
+        OpenAPI openAPI = new OpenAPI();
+        openAPI.setServers(Arrays.asList(server));
+        return openAPI
                 .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
                 .security(Arrays.asList(securityRequirement))
                 .info(new Info()
-                .title("커머스 프로젝트 API")
-                .description("상품을 등록하고, 상품을 장바구니에 담는 기능을 제공합니다.")
+                .title("benefits project - USER-SERVICE API")
+                .description("사용자 회원가입, 로그인 및 프로필, 주소 등록하는 기능을 제공하는 API 입니다.")
                 .version("1.0.0"));
     }
 }
-*/
+
