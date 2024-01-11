815d849a6f0e3e16caefc9e4f19c59cc5c0ec358
Develing1991 <completed0728@gmail.com>
2024-01-11 16:23:55 +0900

    feat: edit space

--- src/main/java/com/benefits/userservice/common/resultcode/UserResultCode.java
+++ src/main/java/com/benefits/userservice/common/resultcode/UserResultCode.java
@@ -10,7 +10,7 @@ public enum UserResultCode implements ResultCodeIfs {
 
     OK(200,200,"성공"),
     CREATE(201,201,"성공"),
-    BAD_REQUEST(HttpStatus.BAD_REQUEST.value(), 400, "잘못 된 요청"),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST.value(), 400, "잘못된 요청"),
     NOT_ACCEPTABLE(HttpStatus.NOT_ACCEPTABLE.value(), 406, "본인과 일치하는 사용자가 아닙니다"),
     NOT_FOUND(HttpStatus.NOT_FOUND.value(), 404, "해당 유저가 존재하지 않습니다"),
 
