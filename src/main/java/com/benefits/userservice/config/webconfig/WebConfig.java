4935d7ab579e5e28ada401f7ee72719d943becf6
Develing1991 <completed0728@gmail.com>
2024-01-11 15:15:40 +0900

    feat: add gatewayDomain, frontDomain env - localhost:8000 게이트웨이 도메인 config 파일로 변경 - production 환경에서 실제 도메인으로 변경할 수 있으니

--- src/main/java/com/benefits/userservice/config/webconfig/WebConfig.java
+++ src/main/java/com/benefits/userservice/config/webconfig/WebConfig.java
@@ -12,15 +12,17 @@ public class WebConfig implements WebMvcConfigurer, EnvironmentAware {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
-        var gatewayIp = "http://" + env.getProperty("gateway.ip") + ":" + env.getProperty("gateway.port");
-        var gatewayLocal = "http://" + env.getProperty("gateway.local") + ":" + env.getProperty("gateway.port");
+        // 보안쿠키는 https, localhost 가 아니면 동작 안하므로 local 환경에서는 ip가 아닌 localhost 사용 권장 - 그러므로 ip 제외
+        var gatewayDomain = env.getProperty("gateway.domain"); // http://localhost:8000 또는 실제 도메인
+        var frontDomain = env.getProperty("front.domain"); // http://localhost:3001 또는 실제 도메인
+
         registry.addMapping("/**") 			// ex) /somePath/**, /user-service/**
                 .allowedMethods("*")				// ex) .allowedMethods("GET", "POST");
                 .allowCredentials(true) // front withCredential true
                 .exposedHeaders("Authorization", "access_token") // front axios res.headers.authorization
-                //.allowedHeaders("x-requested-with", "Authorization", "access_token", "Content-Type")
-                .allowedOrigins(gatewayIp, gatewayLocal, "http://localhost:3001"); // gateway(스웨거), 프론트 허용
+                .allowedOrigins(gatewayDomain, frontDomain); // gateway(스웨거), 프론트 허용
                 // .allowedOriginPatterns("*"); 2.4부터 와일드카드는 allowedOriginPatterns 메소드로 사용 가능
+                //.allowedHeaders("x-requested-with", "Authorization", "access_token", "Content-Type")
     }
 
     @Override
