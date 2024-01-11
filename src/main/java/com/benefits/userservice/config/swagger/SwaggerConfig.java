4ff29165d5f9db4f171bb5dcad000745c30ac58b
Develing1991 <completed0728@gmail.com>
2024-01-11 15:15:21 +0900

    feat: add gatewayDomain env - localhost:8000 게이트웨이 도메인 config 파일로 변경 - production 환경에서 실제 도메인으로 변경할 수 있으니

--- src/main/java/com/benefits/userservice/config/swagger/SwaggerConfig.java
+++ src/main/java/com/benefits/userservice/config/swagger/SwaggerConfig.java
@@ -9,18 +9,20 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import java.util.Arrays;
 
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfig implements EnvironmentAware {
     @Bean
     public ModelResolver modelResolver(ObjectMapper objectMapper){
         return new ModelResolver(objectMapper);
     }
-
+    private Environment env;
     @Bean
     public OpenAPI openAPI(){
         SecurityScheme securityScheme = new SecurityScheme()
@@ -29,7 +31,8 @@ public class SwaggerConfig {
         SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
 
         var server = new Server();
-        server.setUrl("http://localhost:8000/user-service");
+        var gatewayDomain = env.getProperty("gateway.domain");
+        server.setUrl(gatewayDomain + "/user-service");
         OpenAPI openAPI = new OpenAPI();
         openAPI.setServers(Arrays.asList(server));
         return openAPI
@@ -39,6 +42,11 @@ public class SwaggerConfig {
                 .title("benefits project - USER-SERVICE API")
                 .description("사용자 회원가입, 로그인 및 프로필, 주소 등록하는 기능을 제공하는 API 입니다.")
                 .version("1.0.0"));
+    }
+
+    @Override
+    public void setEnvironment(final Environment env) {
+        this.env = env;
     }
 }
 
