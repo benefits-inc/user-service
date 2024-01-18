88ed8af836313ed10942da4fc707c8725cd1721a
Develing1991 <completed0728@gmail.com>
2024-01-19 02:05:06 +0900

    test: edit property yml

--- src/test/java/com/benefits/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/benefits/userservice/UserServiceApplicationTests.java
@@ -9,7 +9,12 @@ import org.springframework.test.context.TestPropertySources;
 
 @SpringBootTest(properties = "spring.config.location=" +
 		"classpath:/application.yaml" +
-		",classpath:/application-*.yml"
+		",classpath:/application-actuator.yml" +
+		",classpath:/application-config.yml" +
+		",classpath:/application-eureka.yml" +
+		",classpath:/application-rabbitmq.yml" +
+		",classpath:/application-swagger.yml" +
+		",classpath:/application-zipkin.yml"
 )
 class UserServiceApplicationTests {
 
