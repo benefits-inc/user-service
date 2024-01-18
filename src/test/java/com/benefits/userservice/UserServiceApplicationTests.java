15dece46be8897f5b123fd6241c1adbd063e561d
Develing1991 <completed0728@gmail.com>
2024-01-19 01:56:36 +0900

    test: delete property yml

--- src/test/java/com/benefits/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/benefits/userservice/UserServiceApplicationTests.java
@@ -6,7 +6,9 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.TestPropertySources;
 
 
-@TestPropertySource(properties = {"spring.config.location = classpath:application*.yml", "spring.config.location = classpath:application*.yaml"})
+@TestPropertySource(properties = {"spring.config.location = classpath:application.yaml",
+		"spring.config.location = classpath:application-actuator.yml",
+		"spring.config.location = classpath:application-config.yml"})
 @SpringBootTest(properties = "spring.profiles.active:prod")
 class UserServiceApplicationTests {
 
