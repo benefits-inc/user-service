19a9ef6584a8526e5f937c4d151b4cb562127547
Develing1991 <completed0728@gmail.com>
2024-01-19 01:41:34 +0900

    test: add test profile prod

--- src/test/java/com/benefits/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/benefits/userservice/UserServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.benefits.userservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active:prod")
 class UserServiceApplicationTests {
 
 	@Test
