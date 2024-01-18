87570f87ced810c280e588eb576fddd5a650e7d8
Develing1991 <completed0728@gmail.com>
2024-01-19 01:55:07 +0900

    test: add properties yml

--- src/test/java/com/benefits/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/benefits/userservice/UserServiceApplicationTests.java
@@ -2,7 +2,11 @@ package com.benefits.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.TestPropertySources;
 
+
+@TestPropertySource(properties = {"spring.config.location = classpath:application*.yml", "spring.config.location = classpath:application*.yaml"})
 @SpringBootTest(properties = "spring.profiles.active:prod")
 class UserServiceApplicationTests {
 
