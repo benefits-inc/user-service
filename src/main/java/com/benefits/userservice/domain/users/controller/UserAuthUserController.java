077e1cf1c9f252c4c7c65a6e4a5a336c244b6bb3
Develing1991 <completed0728@gmail.com>
2024-02-05 09:52:24 +0900

    feat: add auth-user logout api method

--- src/main/java/com/benefits/userservice/domain/users/controller/UserAuthUserController.java
+++ src/main/java/com/benefits/userservice/domain/users/controller/UserAuthUserController.java
@@ -2,12 +2,14 @@ package com.benefits.userservice.domain.users.controller;
 
 import com.benefits.userservice.aop.annotation.UserSelfRole;
 import com.benefits.userservice.common.spec.Api;
+import com.benefits.userservice.domain._auth.business.AuthenticationBusiness;
 import com.benefits.userservice.domain.users.business.UserBusiness;
 import com.benefits.userservice.domain.users.model.UserResponse;
 import com.benefits.userservice.domain.users.model.UserUpdateRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "Users", description = "사용자 정보 AUTH API 입니다. 사용자 자신의 권한(accessToken)이 필요합니다.")
 public class UserAuthUserController {
     private final UserBusiness userBusiness;
+    private final AuthenticationBusiness authenticationBusiness;
 
     @Operation(summary = "사용자 조회 - user_id", description = "등록된 user_id로 개인 사용자 회원 정보를 조회합니다. " +
                                     "<br><br> 개인 사용자 정보이므로 자신만 조회할 수 있습니다.")
@@ -56,5 +59,15 @@ public class UserAuthUserController {
     public Api<UserResponse> deleteUser(@Parameter(example = "1") @PathVariable(name = "id") Long id){
         userBusiness.deleteUser(id);
         return Api.OK(null);
+    }
+
+    @Operation(summary = "사용자 로그아웃", description = "로그아웃 시 redis에 사용하지 않을 accessToken과 refreshToken을 ttl과 함께 등록합니다." +
+            "<br><br> 게이트웨이에서 해당 키를 확인해 존재하면 invalid token으로 처리합니다.")
+    @PostMapping("/logout")
+    public Api<String> logout(HttpServletRequest request){
+
+
+        return authenticationBusiness.logout(request);
+
     }
 }
